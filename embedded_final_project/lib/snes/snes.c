a9fefec07e9c419a0c8f67ff4210c5bbb69410b8
MattQuintana <mq56@nau.edu>
2015-12-10 15:39:50 -0700

    Added snes base files

---
+++ embedded_final_project/lib/snes/snes.c
@@ -0,0 +1,6 @@
+/*
+ * snes.c
+ *
+ * Created: 12/10/2015 3:36:15 PM
+ *  Author: Matt Q
+ */ 
---
+++ embedded_final_project/lib/snes/snes.h
@@ -0,0 +1,16 @@
+/*
+ * snes.h
+ *
+ * Created: 12/10/2015 3:36:42 PM
+ *  Author: Matt Q
+ */ 
+
+
+#ifndef SNES_H_
+#define SNES_H_
+
+
+
+
+
+#endif /* SNES_H_ */
