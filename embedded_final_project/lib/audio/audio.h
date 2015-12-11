70e2a9b4256b91c9d02da9f6da24e694013a8276
Brandon Garling <bjg96@nau.edu>
2015-12-11 13:48:10 -0700

    Improved audio library, added more documentation.

--- embedded_final_project/lib/audio/audio.h
+++ embedded_final_project/lib/audio/audio.h
@@ -11,12 +11,42 @@
 
 #include "lib/fat32/fat32.h"
 
-#define BUFFER_SIZE 256
-#define HALF_BUFFER_SIZE BUFFER_SIZE/2
+// Buffer information
+#define AUDIO_BUFFER_SIZE 512
+#define AUDIO_HALF_BUFFER_SIZE AUDIO_BUFFER_SIZE/2
+
+// Track information
+#define AUDIO_MAX_TRACKS 3
+#define AUDIO_TRACK_0 0
+#define AUDIO_TRACK_1 1
+#define AUDIO_TRACK_2 2
+
+// WAV file header information
+#define AUDIO_WAV_CHUNK_ID_VAL 0x46464952
+#define AUDIO_WAV_CHUNK_ID_POS 0
+
+#define AUDIO_WAV_FORMAT_VAL 0x45564157
+#define AUDIO_WAV_FORMAT_POS 8
+
+#define AUDIO_WAV_SAMPLE_RATE_POS 24
+
+typedef struct _audio_file {
+	fat32_file_t file;
+	uint32_t sample_rate;
+	uint32_t sample_offset;
+	uint8_t is_playing;
+	uint8_t loop;
+	uint8_t last_loaded;
+	volatile uint32_t sample_position;
+	volatile uint8_t buffer[AUDIO_BUFFER_SIZE];
+} audio_file_t;
 
 void audio_init(void);
-void audio_play(fat32_file_t * file);
+void audio_play(char* fname, uint8_t loop, uint8_t track);
 void audio_step();
-
+void audio_pause(uint8_t track);
+void audio_resume(uint8_t track);
+void audio_stop(uint8_t track);
+uint8_t audio_track_is_playing(uint8_t track);
 
 #endif /* AUDIO_H_ */
