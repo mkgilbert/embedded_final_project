8678c4a872ed643c698887e0050d366bde1c5b58
MattQuintana <mq56@nau.edu>
2015-12-10 17:32:36 -0700

    Completed controllers and created an is_pressed function.

--- embedded_final_project/lib/snes/snes.c
+++ embedded_final_project/lib/snes/snes.c
@@ -4,3 +4,68 @@
  * Created: 12/10/2015 3:36:15 PM
  *  Author: Matt Q
  */ 
+
+
+#include "system.h"
+#include <avr/io.h>
+#include <stdio.h>
+#include "lib/port_helpers/port_helpers.h"
+#include "snes.h"
+#include <avr/delay.h>
+
+uint16_t data_c1;
+uint16_t data_c2;
+
+uint8_t snes_is_pressed(uint8_t controller, uint8_t button){
+	if (controller == SNES_CTRL1){
+		return data_c1 & (1<<button) > 0;
+	}
+	else if (controller == SNES_CTRL2){
+		return data_c2 & (1<<button) > 0;
+	}
+	return 0;
+}
+
+void snes_init(){
+	//Set pins as inputs
+	PORT_INPUT (SNES_DATA1);
+	PORT_INPUT (SNES_DATA2);
+	//Set pins as output
+	PORT_OUTPUT (SNES_LATCH);
+	PORT_OUTPUT (SNES_CLOCK);
+	
+	//Enables pull up resistors for each input
+	PORT_SET (SNES_DATA1);
+	PORT_SET (SNES_DATA2);
+	
+	snes_update();
+}
+
+void snes_update(){
+	data_c1 = 0x0000;
+	data_c2 = 0x0000;
+
+	//pull latch to read new data
+	PORT_SET (SNES_LATCH);
+	_delay_us(12);
+	PORT_UNSET (SNES_LATCH);
+	_delay_us(6);
+
+	//read bits
+	for(int i = 0; i < 16; i++){
+		PORT_UNSET (SNES_CLOCK);
+		_delay_us(6);
+		
+		data_c1 |= (PORT_READ (SNES_DATA1) << i);
+		data_c2 |= (PORT_READ (SNES_DATA2) << i);
+		
+		PORT_SET (SNES_CLOCK);
+		_delay_us(6);
+		
+	}
+	
+	data_c1 = ~data_c1;
+	data_c2 = ~data_c2;
+	
+	printf("C1: 0x%04X C2: 0x%04X \n", data_c1, data_c2);
+}
--- embedded_final_project/lib/snes/snes.h
+++ embedded_final_project/lib/snes/snes.h
@@ -9,8 +9,33 @@
 #ifndef SNES_H_
 #define SNES_H_
 
+#define SNES_CLOCK_PIN 1
+#define SNES_CLOCK_PORT A
+#define SNES_LATCH_PIN 2
+#define SNES_LATCH_PORT A
+#define SNES_DATA1_PIN 3
+#define SNES_DATA1_PORT A
+#define SNES_DATA2_PORT A
+#define SNES_DATA2_PIN 4
 
+#define SNES_BTN_B 1 
+#define SNES_BTN_Y 2
+#define SNES_BTN_SELECT 3
+#define SNES_BTN_START 4
+#define SNES_BTN_UP 5
+#define SNES_BTN_DOWN 6
+#define SNES_BTN_LEFT 7
+#define SNES_BTN_RIGHT 8
+#define SNES_BTN_A 9
+#define SNES_BTN_X 10
+#define SNES_BTN_L 11
+#define SNES_BTN_R 12
 
+#define SNES_CTRL1 1
+#define SNES_CTRL2 2
 
+uint8_t snes_is_pressed(uint8_t controller, uint8_t button);
+void snes_init();
+void snes_update();
 
 #endif /* SNES_H_ */
